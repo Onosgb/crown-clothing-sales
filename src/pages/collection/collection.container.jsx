4cd3d9afd0f5c0012cc9c1ab3be98211bab59b5b
Onosgb <onosgb@gmail.com>
2021-12-31 11:20:01 +0100

    fixed bug on the collecton page and collection container

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -2,12 +2,12 @@ import {connect} from 'react-redux';
 
 import { createStructuredSelector } from 'reselect';
 import WithSpinner from '../../components/with-spinner/with-spinner';
-import {selectIsCollectionsLoaded} from '../../redux/shop/shop.selector';
+import {selectCollectionIsFetching} from '../../redux/shop/shop.selector';
 import CollectionPage from './collecton.component'
 import {compose} from 'redux';
 
 const mapStateToProps = createStructuredSelector({
-isLoading: state => !selectIsCollectionsLoaded(state)
+isLoading:  selectCollectionIsFetching
 })
 
 const CollectionPageContainer = compose(
--- src/pages/collection/collecton.component.jsx
+++ src/pages/collection/collecton.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import {selectCollection} from '../../redux/shop/shop.selector'
+
 import './collection.styles.scss'
 
 const CollectionPage = ({collection}) => {
@@ -17,5 +19,7 @@ const CollectionPage = ({collection}) => {
         </div>
 )}
 
-
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+export default connect(mapStateToProps)(CollectionPage)
